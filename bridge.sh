c67b2412c927c1fdb9ccd1d8a13943b1346337d7
bobbybee <j104@rocketmail.com>
2014-12-12 12:28:41 -0500

    Begin bridge

---
+++ bridge.sh
@@ -0,0 +1,11 @@
+# bridge creates a virtual network bridge between the Scratch extension in the browser
+# and the physical network using a WebSocket server and backpiped hexinject
+# it is hyper implementation specific... use with caution
+
+#mkfifo backpipe
+#node webcat.js -lwqb < backpipe | sudo hexinject -p -i en0 &
+#sudo hexinject -s -i en0 > backpipe
+
+mkfifo backpipe
+node webcat.js -lwqb < backpipe
+sudo hexinject -s -i en0 > backpipe
