ea610650a345791f43ac322bf901bbcedc9919e2
bobbybee <j104@rocketmail.com>
2014-12-12 12:49:04 -0500

    Data available for queueing

--- extension.js
+++ extension.js
@@ -1,4 +1,6 @@
 (function(ext) {
+	var data = "";
+	var readyForData = true;
 
 	ext._shutdown = function() {};
 	
@@ -9,13 +11,30 @@
 	ext.connect = function() {
 		var socket = new WebSocket("ws://localhost:8080/");
 		socket.onmessage = function(d) {
-			console.log(d.data);
+			data += d.data.trim() + " ";
+			//console.log(d.data);
 		}
+	};
+
+	ext.when_data = function() {
+		if(readyForData && data.length > 1) {
+			readyForData = false;
+			return true;
+		}
+
+		return false;
+	};
+
+	ext.dataReady = function() {
+		readyForData = true;
 	};
 
 	var descriptor = {
 		blocks: [
 			[' ', 'connect', 'connect'],
+			['-'],
+			['h', 'when data is available', 'when_data'],
+			[' ', 'ready for data', 'dataReady'],
 		]
 	};
 
