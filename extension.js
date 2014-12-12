da04648c042238a28655cc5cfd2f722acb237955
bobbybee <j104@rocketmail.com>
2014-12-12 12:40:16 -0500

    Extension connects; packets are flooding in by the dozen

--- extension.js
+++ extension.js
@@ -1,12 +1,21 @@
 (function(ext) {
+
 	ext._shutdown = function() {};
 	
 	ext._getStatus = function() {
 		return { status: 2, msg: 'Ready' };
 	};
 
+	ext.connect = function() {
+		var socket = new WebSocket("ws://localhost:8080/");
+		socket.onmessage = function(d) {
+			console.log(d.data);
+		}
+	};
+
 	var descriptor = {
 		blocks: [
+			[' ', 'connect', 'connect'],
 		]
 	};
 
